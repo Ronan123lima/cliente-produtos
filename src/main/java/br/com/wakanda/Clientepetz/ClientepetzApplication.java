664218224244546f9ccca6c9ec055489dd3bb683
Ronan Lima <ronanlimasilva@hotmail.com>
2024-05-19 09:55:11 -0300

    inicia o projeto

---
+++ src/main/java/br/com/wakanda/Clientepetz/ClientepetzApplication.java
@@ -0,0 +1,13 @@
+package br.com.wakanda.Clientepetz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientepetzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientepetzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/wakanda/Clientepetz/ClientepetzApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wakanda.Clientepetz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientepetzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
