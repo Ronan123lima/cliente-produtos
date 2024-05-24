d7e060f2772f7b519f291677185a4d33010f4ee8
Ronan Lima <ronanlimasilva@hotmail.com>
2024-05-24 14:49:09 -0300

    implementa request e response

--- src/main/java/br/com/copysul/Clienteproduto/cliente/application/api/ClienteRequest.java
+++ src/main/java/br/com/copysul/Clienteproduto/cliente/application/api/ClienteRequest.java
@@ -1,5 +1,26 @@
 package br.com.copysul.Clienteproduto.cliente.application.api;
 
-public class ClienteRequest {
+import org.hibernate.validator.constraints.br.CPF;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Value;
+
+@Value
+public class ClienteRequest {
+	@NotBlank
+	private String nomeCompleto;
+	@NotBlank
+	@Email
+	private String email;
+	@NotBlank
+	private String celular;
+	private String telefone;
+	@NotNull
+	private String dataNascimento;
+	@CPF
+	private String cpf;
+	@NotNull
+	private String aceitaTermos;
 }
