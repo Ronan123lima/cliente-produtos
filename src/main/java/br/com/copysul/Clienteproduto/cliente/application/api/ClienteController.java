0720ea086bc220108e0d0dddc4a8bf8016b2b5f0
Ronan Lima <ronanlimasilva@hotmail.com>
2024-06-04 19:34:45 -0300

    implementa fluxo de servico de aplicacao

--- src/main/java/br/com/copysul/Clienteproduto/cliente/application/api/ClienteController.java
+++ src/main/java/br/com/copysul/Clienteproduto/cliente/application/api/ClienteController.java
@@ -35,8 +35,9 @@ public class ClienteController implements ClienteApi {
 	public ClienteDetalhadoResponse getClientesAtravesId(UUID idCliente) {
 		log.info("[inicia] ClienteController - getClientesAtravesId");
 		log.info("[idCliente]{}",idCliente);
+		ClienteDetalhadoResponse clienteDetalhado = clienteService.buscaClienteAtravesId(idCliente);
 		log.info("[finaliza] ClienteController - getClientesAtravesId");
-		return null;
+		return clienteDetalhado;
 	}
 
 }
--- src/main/java/br/com/copysul/Clienteproduto/cliente/application/service/ClienteService.java
+++ src/main/java/br/com/copysul/Clienteproduto/cliente/application/service/ClienteService.java
@@ -1,7 +1,9 @@
 package br.com.copysul.Clienteproduto.cliente.application.service;
 
 import java.util.List;
+import java.util.UUID;
 
+import br.com.copysul.Clienteproduto.cliente.application.api.ClienteDetalhadoResponse;
 import br.com.copysul.Clienteproduto.cliente.application.api.ClienteListResponse;
 import br.com.copysul.Clienteproduto.cliente.application.api.ClienteRequest;
 import br.com.copysul.Clienteproduto.cliente.application.api.ClienteResponse;
@@ -9,5 +11,6 @@ import br.com.copysul.Clienteproduto.cliente.application.api.ClienteResponse;
 public interface ClienteService {
 	ClienteResponse criaCliente(ClienteRequest clienteRequest);
 	List<ClienteListResponse> buscaTodosClientes();
+	ClienteDetalhadoResponse buscaClienteAtravesId(UUID idCliente);
 }
  
